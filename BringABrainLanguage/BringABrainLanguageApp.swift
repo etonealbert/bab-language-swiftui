9b1318d1b63ed829d332c9550d97b2b4c8ea2241
Mary Bogert <mary.bogert@cointelegraph.com>
2026-02-06 12:14:12 -0300

    feat: integrate SwiftData schema into app entry point with ModelContainer

--- BringABrainLanguage/BringABrainLanguageApp.swift
+++ BringABrainLanguage/BringABrainLanguageApp.swift
@@ -10,9 +10,30 @@ import SwiftData
 
 @main
 struct BringABrainLanguageApp: App {
+    
+    let modelContainer: ModelContainer
+    
+    init() {
+        do {
+            let schema = Schema(BabLanguageSchemaV1.models)
+            let modelConfiguration = ModelConfiguration(
+                schema: schema,
+                isStoredInMemoryOnly: false
+            )
+            modelContainer = try ModelContainer(
+                for: schema,
+                migrationPlan: BabLanguageMigrationPlan.self,
+                configurations: [modelConfiguration]
+            )
+        } catch {
+            fatalError("Could not initialize ModelContainer: \(error)")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(modelContainer)
     }
 }
